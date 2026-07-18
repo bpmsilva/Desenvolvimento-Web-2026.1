ce756acbe7b531ce85e5dcae9fd19e273b475483
Bernardo <bpmsilva@gmail.com>
2026-07-18 15:08:44 -0300

    aula de 2026-07-11

---
+++ aula-xx-data-2026-07-11/javascript/03-variaveis.js
@@ -0,0 +1,6 @@
+'strict mode';
+
+var nome = 'João' // var em um escopo "estranho" e deve ser evitada
+let idade = 18    // let tem um escopo mais restrito e é uma modificador mais recente
+const PI = 3.14   // const é uma constante que "sempre" existiu
+x = 1;            // cria uma variável global mas não funciona no 'strict mode'
---
+++ aula-xx-data-2026-07-11/javascript/scripts/arquivo.js
@@ -0,0 +1,5 @@
+console.log('Olá, mundo!');
+console.log('Olá, mundo! 3');
+
+/* Eu modifique o h1 do corpo do HTML (linha 14) */
+document.querySelector('h1').style.color = 'red';
